dc61c1af989e85010fff697adc9f5ab962bbd01e
vrajesh <vrajesh2badgujar@gmail.com>
2022-07-21 13:02:03 +0530

    password is changed with secret key

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,10 +1,22 @@
 const mongoose  = require("mongoose")
-mongoose.connect("mongodb://localhost:27017/vrajeshdynamic",{
-    // useCreateIndex: true,
+// mongoose.connect("mongodb://localhost:27017/vrajeshdynamic",{
+//     // useCreateIndex: true,
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+// }).then(()=>{
+//     console.log("successful connection");
+// }).catch((error)=>{
+//     console.log(error);
+// })
+
+
+const DB ="mongodb+srv://vrajesh:123123vmb@cluster0.qybgzts.mongodb.net/vrajeshdynamic?retryWrites=true&w=majority"
+
+mongoose.connect(DB, {
     useNewUrlParser:true,
-    useUnifiedTopology:true
+    useUnifiedTopology:true,
+    // useFindAndModify:false,
+    // useCreateIndex:true
 }).then(()=>{
-    console.log("successful connection");
-}).catch((error)=>{
-    console.log(error);
-})
+    console.log(`connection successful`);
+}).catch((err)=>console.log(`no connection`));
